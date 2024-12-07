c0f7b8f0200d46345d8e126b651661d7b36337b6
PranavaSwarup <pranava.swarup1703@gmail.com>
2024-12-07 17:15:07 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import Image, ImageTk  # Import Image and ImageTk from Pillow
+from csv import *
+from test17 import *
+        
+
+
+# Create the initial application window
+root = Tk()
+
+# Create an instance of the QuizSocApp class
+app = QuizSocApp(root)
+
+# Run the application
+root.mainloop()
+
+
